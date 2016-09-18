1a5d958d4ef7519adfc20d2df4f99a602999cc42
drwiner <drwiner131@gmail.com>
2016-09-18 16:51:50 -0400

    added cinematic model data component

---
+++ FireBoltUnity/Assets/FireBolt/scripts/CinematicModelMetaDataComponent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.scripts
+{
+    public class CinematicModelMetaDataComponent : MonoBehaviour
+    {
+        public string ModelName;
+        public string ActorName;
+        public string AbstractActorName;
+        public float PointOfInterestScalar;
+
+        public void LoadFromStruct(CinematicModelMetaData existingData)
+        {
+            this.ModelName = existingData.ModelName;
+            this.ActorName = existingData.ActorName;
+            this.AbstractActorName = existingData.AbstractActorName;
+            this.PointOfInterestScalar = existingData.PointOfInterestScalar;
+        }
+    }
+
+    public struct CinematicModelMetaData
+    {
+        public string ModelName;
+        public string ActorName;
+        public string AbstractActorName;
+        public float PointOfInterestScalar;
+    }
+}
